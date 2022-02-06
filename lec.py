be960854d1ba8e8233c033a10dd61c7dfa4d8bcd
Evgenia <kolontai.e.v@yandex.ru>
2022-02-06 20:12:22 +0300

    По двум заданным числам проверить является ли одно квадратом второго

---
+++ lec.py
@@ -0,0 +1,95 @@
+# Ввод и вывод данных
+# print('Введите а');
+# a = input() # 10
+# print('Введите b');
+# b = input() # 20
+# c = 30
+# print(a, ' + ', b, ' = ', c)
+
+# Ввод и вывод данных
+# print('Введите а');
+# a = int(input())
+# print('Введите b');
+# b = int(input())
+# c = 30
+# print(a, ' + ', b, ' = ', c)
+# print('{} + {} = {}'.format(a, b, c))
+
+# # Ввод и вывод данных
+# print('Введите а');
+# a = float(input())
+# print('Введите b');
+# b = float(input())
+# c = ..
+# print(a, ' + ', b, ' = ', c)
+
+
+# exp1 = 2**3 - 10 % 5 + 2*3
+# exp2 = 2**3 - 10 / 5 + 2*3
+# print(exp1) # 14.0 или 14
+# print(exp2) # 12.0 или 12
+
+# iter = 2
+# iter += 3 # iter = iter + 3
+# # iter -= 4 # iter = iter - 4
+# # iter *= 5 # iter = iter * 5
+# # iter /= 5 # iter = iter / 5
+# # iter //= 5 # iter = iter // 5
+# # iter %= 5 # iter = iter % 5
+# # iter **= 5 # iter = iter ** 5
+
+# a, b, c = 1, 2, 3
+# # a: 1 b: 2 c: 3
+# print('a: {} b: {} c: {}'.format(a, b, c))
+# range(*(1,5,2))
+
+# username = input('Введите имя: ')
+# if(username == 'Маша'):
+#  print('Ура, это же МАША!');
+# else:
+#  print('Привет, ', username);
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#  print('Ура, это же МАША!')
+# elif username == 'Марина':
+#  print('Я так ждала Вас, Марина!')
+# elif username == 'Ильнар':
+#  print('Ильнар - топ)')
+# else:
+#  print('Привет, ', username)
+
+# original = 53
+# inverted = 0
+# while original != 0:
+#  inverted = inverted * 10 + (original % 10)
+#  original //= 10
+# print(inverted)
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#  inverted = inverted * 10 + (original % 10)
+#  original //= 10
+# else:
+#  print('Пожалуй')
+#  print('хватит )')
+# print(inverted)
+# # Пожалуй
+# # хватит )
+# # 32
+
+
+# for i in 1, -2, 3, 14, 5:
+#     print(i)
+# # 1
+# # -2
+# # 3
+# # 14
+# # 5
+
+r = range(5) # range(0, 5)
+# r = range(2, 5) # range(2, 5)
+# r = range(-5, 0) # range(-5, 0)
+# r = range(1, 10, 2) # range(1, 10, 2)
+# r = range(100, 0, -20) # range(100, 0, -20)
